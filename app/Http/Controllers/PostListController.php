f986c0c4a55146ebeb0849b3aa90536c8844cffd
nshiro <14008307+nshiro@users.noreply.github.com>
2022-03-12 15:50:09 +0000

    ブログテーブルの用意

--- app/Http/Controllers/PostListController.php
+++ app/Http/Controllers/PostListController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class PostListController extends Controller
 {
-    // public function index()
-    // {
+    public function index()
+    {
 
-    // }
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Post::factory(30)->create();
     }
 }
