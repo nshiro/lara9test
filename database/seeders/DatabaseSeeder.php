0da2f170aa2be9852699faeb2bb1167dff5164b4
nshiro <14008307+nshiro@users.noreply.github.com>
2022-03-14 02:17:46 +0000

    Userが複数Postを所有する際のDatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Nette\Utils\Random;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +19,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Post::factory(30)->create();
+        // Post::factory(30)->create();
+
+        User::factory(15)->create()->each(function ($user) {
+            Post::factory(random_int(2, 5))->create(['user_id' => $user]);
+        });
     }
 }
