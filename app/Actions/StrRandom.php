3888880a85c58aa4c61170f677906b8e1f108adc
nshiro <14008307+nshiro@users.noreply.github.com>
2022-04-01 09:12:04 +0000

    aliaseについて

--- app/Actions/StrRandom.php
+++ app/Actions/StrRandom.php
@@ -2,10 +2,13 @@
 
 namespace App\Actions;
 
+use Illuminate\Support\Str;
+
 class StrRandom
 {
     public function get($length)
     {
-        return \Str::random($length);
+        // return \Str::random($length);
+        return Str::random($length);
     }
 }
